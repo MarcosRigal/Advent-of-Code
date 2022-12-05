5e272bbbe35a5d56199cf2fb01d52cb47921cd7d
MarcosRigal <riveragavilanmarcos@gmail.com>
2022-12-05 08:59:21 +0100

    2022 on Rust

---
+++ 2022/Day1/target/debug/Day1.d
@@ -0,0 +1,1 @@
+/home/marcos/Repositories/Advent-of-Code/2022/Day1/target/debug/Day1: /home/marcos/Repositories/Advent-of-Code/2022/Day1/src/main.rs
---
+++ 2022/Day1/target/debug/deps/Day1-07205dc409ccd5ce.d
@@ -0,0 +1,5 @@
+/home/marcos/Repositories/Advent-of-Code/2022/Day1/target/debug/deps/Day1-07205dc409ccd5ce: src/main.rs
+
+/home/marcos/Repositories/Advent-of-Code/2022/Day1/target/debug/deps/Day1-07205dc409ccd5ce.d: src/main.rs
+
+src/main.rs:
